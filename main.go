05cbd58cf1b3ceec8ae118334112bc482a512090
Sandip Arsude <arsudesandip4@gmail.com>
2024-01-08 12:00:30 +0530

    testing receiver function

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 
 	cards= append(cards, "Six of Spades")
-	for i, cards := range cards{
-		fmt.Println(i, cards)
-
-	}
+	cards.print()
 }
 
 func newCard() string {
