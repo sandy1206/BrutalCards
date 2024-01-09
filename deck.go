30404d72626087a00bd5b27059440a9972b09b2f
Sandip Arsude <arsudesandip4@gmail.com>
2024-01-09 12:00:30 +0530

    added new deck creation method

--- deck.go
+++ deck.go
@@ -7,8 +7,22 @@ import "fmt"
 
 type deck []string
 
-func (d deck) print(){
-	for i, card := range d{
-		fmt.Println(i,card)
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"spades", "diamonds", "hearts", "clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
 	}
 }
