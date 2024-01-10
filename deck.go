2d790320ea41b9b88b956046021ed8b524515826
Sandip Arsude <arsudesandip4@gmail.com>
2024-01-10 12:00:30 +0530

    added func for string to byte conversation

--- deck.go
+++ deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // create a new type of deck
 // which is a slice of strings
@@ -25,4 +28,14 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) deal(handSize int) (deck, deck) {
+	hand := d[:handSize]
+	remainingCards := d[handSize:]
+	return hand, remainingCards
+}
+
+func (d deck) toByteString() []byte {
+	return []byte(strings.Join(d, ""))
 }
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 	cards.print()
+	hand, remainingCards := cards.deal(5)
+	fmt.Println(hand, "------------------------------------", remainingCards)
 }
